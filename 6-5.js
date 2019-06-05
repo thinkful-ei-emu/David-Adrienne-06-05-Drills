72eae7a2e075dbcb58965adb901446c494a7812a
David Omstead <davidomstead@hotmail.com>
2019-06-05 11:20:32 -0700

    Drill Work

---
+++ 6-5.js
@@ -0,0 +1,20 @@
+function createMyObject(){
+    return{
+    foo: "bar",
+    answerToUniverse: 42,
+    "olly olly": "oxen free",
+    sayHello: function(){
+      return "hello"
+    },
+  };
+  }
+  console.log(createMyObject)
+  
+  
+  function updateObject(obj) {
+    obj.foo = 'foo';
+    obj.bar = 'bar';
+    obj.bizz = 'bizz';
+    obj.bang = 'bang';
+    return obj;
+  }
